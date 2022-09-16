d995c4149e802d3b571611bd20d011b022857a90
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-16 15:10:29 -0600

    working on it

--- app.js
+++ app.js
@@ -2,18 +2,70 @@
 import { getRandomItem } from './utils.js';
 
 /* State */
-let shell = ['shell-1', 'shell-2', 'shell-3'];
+// let shell = ['shell-1', 'shell-2', 'shell-3'];
 let pearl = ['pearl-1', 'pearl-2', 'pearl-3'];
-let guess = ['guess-1', 'guess-2', 'guess-3'];
-let display = ['display-1', 'display-2', 'display-3'];
+// let guess = ['guess-1', 'guess-2', 'guess-3'];
+const display1 = document.getElementById('display-1');
+const display2 = document.getElementById('display-2');
+const display3 = document.getElementById('display-3');
+
+const guess1 = document.getElementById('guess-1');
+const guess2 = document.getElementById('guess-2');
+const guess3 = document.getElementById('guess-3');
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
 
 const gameState = ['guess', 'results'];
 
-guess.addEventListener('click', () => {});
-/* Actions */
+// let guess = [guess1, guess2, guess3];
+
 function loadPage() {
+    displayShells();
     getRandomItem.pearl;
+    pearl.classList.remove('hidden');
+    gameState = 'guess';
 }
+
+guess1.addEventListener('click', () => {
+    guess = guess1;
+    gameState = 'results';
+    shell1.classList.add('reveal');
+});
+
+guess2.addEventListener('click', () => {
+    guess = guess2;
+    gameState = 'results';
+    shell2.classList.add('reveal');
+});
+
+guess3.addEventListener('click', () => {
+    guess = guess3;
+    gameState = 'results';
+    shell3.classList.add('reveal');
+});
+
+function displayShells() {
+    if ((gameState = 'guess')) {
+        pearl1.classList.add('hidden');
+        pearl2.classList.add('hidden');
+        pearl3.classList.add('hidden');
+        shell1.classList.remove('reveal');
+        shell2.classList.remove('reveal');
+        shell3.classList.remove('reveal');
+        display1.classList.add('hidden');
+        display2.classList.add('hidden');
+        display3.classList.add('hidden');
+        playAgainButton.classList.add('hidden');
+    }
+}
+
+// function choose(userGuess) {
+//     gameState = 'results';
+//     guess = userGuess;
+//     reveal = pearl;
+//     displayPearl();
+// }
 
 /* Components */
 
