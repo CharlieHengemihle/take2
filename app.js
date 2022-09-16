280768eb4f53bf37ee97c5bd157ba5b135eb5d26
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-16 11:43:22 -0600

    shell game take 2

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 /* Imports */
-// import { getRandomItem } from './utils.js';
+import { getRandomItem } from './utils.js';
 
 /* State */
+let shell = ['shell-1', 'shell-2', 'shell-3'];
+let pearl = ['pearl-1', 'pearl-2', 'pearl-3'];
+let guess = ['guess-1', 'guess-2', 'guess-3'];
+let display = ['display-1', 'display-2', 'display-3'];
 
+const gameState = ['guess', 'results'];
+
+guess.addEventListener('click', () => {});
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    getRandomItem.pearl;
+}
 
 /* Components */
 
